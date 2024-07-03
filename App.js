fb3e1ecbad5642c34cbf9d786e50dd500c8a32de
NEVI0 <neviocostamagagnin@gmail.com>
2024-07-02 21:27:08 -0300

    feat: adicionando app.js

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
-export default function App() {
+import { useEffect } from 'react';
+import { Text, View, LogBox  } from 'react-native';
+
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+import { Button, Input } from './src/components';
+import { theme } from './src/theme';
+
+LogBox.ignoreAllLogs();
+SplashScreen.preventAutoHideAsync();
+
+const App = () => {
+  const [loaded, error] = useFonts({
+    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+    'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'Inter-ExtraBold': require('./assets/fonts/Inter-ExtraBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{height: 1200, gap: 64, paddingTop: 64, paddingHorizontal: 32, backgroundColor: theme.colors.container.main}}>
+      <Text>fdfdsfsd</Text>
+
+      <Input
+        label="E-mail"
+        placeholder="exemplo@gmail.com"
+        icon="mail"
+        // value="E-mail"
+        // onChangeText="E-mail"
+      />
+
+      <Input
+        label="Senha"
+        icon="lock"
+        type={{}}
+        // value="E-mail"
+        // onChangeText="E-mail"
+      />
+
+      <Button text={'Acessar minha conta'} icon={'log-in'} onPress={() => console.log('dfdfde')}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
