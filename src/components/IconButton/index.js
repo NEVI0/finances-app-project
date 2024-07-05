c0942f9c76c0b63300859f2edb01c3251dd73a30
NEVI0 <neviocostamagagnin@gmail.com>
2024-07-04 21:37:50 -0300

    feat: removendo opacidade no click do botão

--- src/components/IconButton/index.js
+++ src/components/IconButton/index.js
@@ -4,7 +4,7 @@ import { Icon } from '../Icon';
 
 export const IconButton = ({ icon, color, onPress }) => {
     return (
-        <TouchableOpacity onPress={onPress}>
+        <TouchableOpacity onPress={onPress} activeOpacity={1}>
             <Icon name={icon} color={color} />
         </TouchableOpacity>
     );
