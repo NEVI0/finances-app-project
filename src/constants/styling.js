f7f13115708b64c5f3528969ad37a0168ea49a57
NEVI0 <neviocostamagagnin@gmail.com>
2024-07-08 22:25:22 -0300

    refactor: mudando constants

---
+++ src/constants/styling.js
@@ -0,0 +1,15 @@
+import { Platform, StatusBar } from 'react-native';
+
+const TOP_BAR_HEIGHT = Platform.OS === 'android' ? StatusBar.currentHeight : 20;
+const BOTTOM_BAR_HEIGHT = 28;
+const CONTAINER_SPACING = 32;
+const SECTION_SPACING = 16;
+const SUBSECTION_SPACING = 16;
+
+export const STYLING = {
+    TOP_BAR_HEIGHT,
+    BOTTOM_BAR_HEIGHT,
+    CONTAINER_SPACING,
+    SECTION_SPACING,
+    SUBSECTION_SPACING,
+}
--- src/constants/topBarHeight.js
+++
@@ -1,3 +0,0 @@
-import { Platform, StatusBar } from 'react-native';
-
-export const TOP_BAR_HEIGHT = Platform.OS === 'android' ? StatusBar.currentHeight : 20;
