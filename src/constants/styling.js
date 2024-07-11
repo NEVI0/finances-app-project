634142c7f31a40c8586a43ace6d754af2977579f
NEVI0 <neviocostamagagnin@gmail.com>
2024-07-10 21:48:37 -0300

    refactor: mudando o valor do spacing

--- src/constants/styling.js
+++ src/constants/styling.js
@@ -4,7 +4,7 @@ const TOP_BAR_HEIGHT = Platform.OS === 'android' ? StatusBar.currentHeight : 20;
 const BOTTOM_BAR_HEIGHT = 28;
 const CONTAINER_SPACING = 32;
 const SECTION_SPACING = 16;
-const SUBSECTION_SPACING = 16;
+const SUBSECTION_SPACING = 8;
 
 export const STYLING = {
     TOP_BAR_HEIGHT,
