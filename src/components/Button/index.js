5127cdea24ad68666e42709f53097f296f106f8f
NEVI0 <neviocostamagagnin@gmail.com>
2024-07-25 21:07:24 -0300

    feat: adicionando propriedade de loading ao botão

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,21 +1,41 @@
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 import { Icon } from '../Icon';
 
 import { theme } from '../../theme';
 import { styles } from './styles';
 
-export const Button = ({ text, icon, iconColor = theme.colors.primary, type = 'primary', onPress }) => {
+export const Button = ({
+    text,
+    icon,
+    iconColor = theme.colors.primary,
+    type = 'primary',
+    loading = false,
+    onPress,
+}) => {
     return (
-        <TouchableOpacity style={styles[type].button} activeOpacity={0.5} onPress={onPress}>
+        <TouchableOpacity
+            style={styles[type].button}
+            activeOpacity={0.5}
+            disabled={loading}
+            onPress={onPress}
+        >
             <Text style={styles[type].text}>
                 {text}
             </Text>
 
-            <Icon
-                name={icon}
-                color={type === 'primary' ? theme.colors.text.main : iconColor}
-            />
+            {
+                loading ? (
+                    <ActivityIndicator
+                        color={type === 'primary' ? theme.colors.text.main : iconColor}
+                    />
+                ) : (
+                    <Icon
+                        name={icon}
+                        color={type === 'primary' ? theme.colors.text.main : iconColor}
+                    />
+                )
+            }
         </TouchableOpacity>
     );
 }
