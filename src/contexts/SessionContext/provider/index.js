86ba2727bee000813a7f4bb19a3f51d80b157242
NEVI0 <neviocostamagagnin@gmail.com>
2024-07-25 22:47:56 -0300

    feat: adicionando funções de signin e signup do context de sessão

--- src/contexts/SessionContext/provider/index.js
+++ src/contexts/SessionContext/provider/index.js
@@ -1,11 +1,60 @@
 import { useState } from 'react';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-toast-message';
+
+import { createUserService, fetchUserDataService } from '../../../services/user';
+import { signupUserService, signinUserService } from '../../../services/auth';
+
+
 import { SessionContext } from '../hooks/useSession';
 
 export const SessionProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
+    const signin = async (data) => {
+        try {
+            const { email, password } = data;
+            const user = await signinUserService(email, password);
+            const userData = await fetchUserDataService(user.uid);
+            
+            delete userData.password;
+            const toSaveData = { ...userData, authUid: user.uid };
+
+            await AsyncStorage.setItem('user', JSON.stringify(toSaveData));
+            setUser(toSaveData);
+        } catch (error) {
+            Toast.show({
+                type: 'error',
+                text1: 'Alerta!',
+                text2: error.message,
+            });
+        }
+    }
+
+    const signup = async (data) => {
+        try {
+            const { email, password } = data;
+            const user = await signupUserService(email, password);
+            
+            delete data.password;
+            const toSaveData = { ...data, authUid: user.uid };
+
+            await createUserService(toSaveData);
+            await AsyncStorage.setItem('user', JSON.stringify(toSaveData));
+
+            setUser(toSaveData);
+        } catch (error) {
+            Toast.show({
+                type: 'error',
+                text1: 'Alerta!',
+                text2: error.message,
+            });
+        }
+    }
+
     return (
-        <SessionContext.Provider value={{ user, setUser }}>
+        <SessionContext.Provider value={{ user, signin, signup, setUser }}>
             {children}
         </SessionContext.Provider>
     );
